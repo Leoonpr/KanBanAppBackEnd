f569bde60d3090adc6700fbe5058f68f39001af4
leonardo <rochaleo879@gmail.com>
2023-09-03 23:44:22 -0300

    feat: add routes

--- src/server.js
+++ src/server.js
@@ -1,10 +1,11 @@
 const express = require("express");
+
+const routes = require("./routes");
+
 const app = express();
+app.use(express.json);
+app.use(routes);
 const PORT = 3333;
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`);
-});
-
-app.get("/", (req, res) => {
-  res.send("Hello, world!");
 });
