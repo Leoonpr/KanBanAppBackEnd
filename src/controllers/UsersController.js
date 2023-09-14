39943b4a0364c6299c4e2ed43ef885cb351d880a
leonardo <rochaleo879@gmail.com>
2023-09-13 22:41:04 -0300

    feat: cria o update na UsersController

--- src/controllers/UsersController.js
+++ src/controllers/UsersController.js
@@ -21,7 +21,39 @@ class UsersController {
       response.status(500).json({ message: "Erro ao criar usuário" });
     }
   }
-  
+
+  async update(request, response) {
+    const { id } = request.params; // Supondo que você está passando o ID do usuário como parâmetro na URL
+    const { name, email, password } = request.body;
+
+    try {
+      const existingUser = await knex("user").where({ id }).first();
+
+      if (!existingUser) {
+        return response.status(404).json({ message: "Usuário não encontrado" });
+      }
+
+      if (email !== existingUser.email) {
+        // O email é diferente, então podemos atualizar
+        await knex("user").where({ id }).update({
+          name,
+          email,
+          password,
+        });
+
+        response
+          .status(200)
+          .json({ message: "Usuário atualizado com sucesso" });
+      } else {
+        response
+          .status(400)
+          .json({ message: "O email não pode ser igual ao antigo" });
+      }
+    } catch (error) {
+      console.error(error);
+      response.status(500).json({ message: "Erro ao atualizar usuário" });
+    }
+  }
 }
 
 module.exports = UsersController;
